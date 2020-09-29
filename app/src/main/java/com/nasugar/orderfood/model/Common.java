7cbc7dee150980b13b590fb9654c2e9c16a54144
vietdung1808 <vietdung1808@gmail.com>
2020-09-29 08:36:01 +0700

    login

---
+++ app/src/main/java/com/nasugar/orderfood/model/Common.java
@@ -0,0 +1,6 @@
+package com.nasugar.orderfood.model;
+
+public class Common {
+    public static final String USER_KEY = "User";
+    public static final String PWD_KEY = "Password";
+}
--- app/src/main/java/com/nasugar/orderfood/view/LoginActivity.java
+++ app/src/main/java/com/nasugar/orderfood/view/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.nasugar.orderfood.view;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -18,19 +17,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.nasugar.orderfood.R;
+import com.nasugar.orderfood.model.Common;
 import com.nasugar.orderfood.model.User;
+
+import io.paperdb.Paper;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,37 +41,43 @@ public class LoginActivity extends AppCompatActivity {
     DatabaseReference mData;
     FirebaseAuth mAuthencation;
     Button btnLog;
-    EditText email;
-    EditText pass;
+    EditText etEmail;
+    EditText etPass;
 
-    CheckBox Remember;
-    TextView forgotPass;
+    CheckBox chkRemember;
+    TextView tvForgotPass;
 
-    AlertDialog waiting;
+    AlertDialog dlgWaiting;
 
-
-
-
+    String mEmail, mPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
-        AnhXa();
-//        waiting =  new SpotsDialog.Builder().setContext(this).setMessage("Vui lòng đợi...").setCancelable(false).build();
+        mapViews();
+        dlgWaiting = new AlertDialog.Builder(this).setMessage("Vui lòng đợi...").setCancelable(false).create();
         mAuthencation = FirebaseAuth.getInstance();
 
-//        Paper.init(this);
+        Paper.init(this);
+        mEmail = Paper.book().read(Common.USER_KEY);
+        mPass = Paper.book().read(Common.PWD_KEY);
+        if (mEmail.length() > 0 && mPass.length() > 0) {
+            DangNhap(mEmail, mPass, true);
+        }
 
         btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DangNhap();
+                mEmail = etEmail.getText().toString().trim();
+                mPass = etPass.getText().toString().trim();
+                DangNhap(mEmail, mPass, false);
             }
         });
 
-        forgotPass.setOnClickListener(new View.OnClickListener() {
+
+        tvForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                startActivity(new Intent(LoginActivity.this,ForgotPassActivity.class));
@@ -77,27 +85,25 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void AnhXa() {
-        btnLog = (Button) findViewById(R.id.btnLog);
-        email = (EditText) findViewById(R.id.edtEmailLog);
-        pass = (EditText) findViewById((R.id.edtPassLog));
-        Remember = (CheckBox) findViewById(R.id.ckbRemember);
-        forgotPass = (TextView) findViewById(R.id.forgotPass);
+    private void mapViews() {
+        btnLog = findViewById(R.id.btnLog);
+        etEmail = findViewById(R.id.edtEmailLog);
+        etPass = findViewById((R.id.edtPassLog));
+        chkRemember = findViewById(R.id.ckbRemember);
+        tvForgotPass = findViewById(R.id.forgotPass);
     }
 
-    private void DangNhap() {
-        final String Email = email.getText().toString().trim();
-        final String Pass = pass.getText().toString().trim();
-        if (Email.isEmpty() || Pass.isEmpty()) {
+    private void DangNhap(String email, final String pass, final boolean auto_login) {
+        if (email.isEmpty() || pass.isEmpty()) {
             Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin. ", Toast.LENGTH_SHORT).show();
         } else {
             if (isNetworkAvailable()) {
-//                waiting.show();
-//                if (Remember.isChecked()) {
-//                    Paper.book().write(Common.USER_KEY, Email);
-//                    Paper.book().write(Common.PWD_KEY, Pass);
-//                }
-                mAuthencation.signInWithEmailAndPassword(Email, Pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                dlgWaiting.show();
+                if (chkRemember.isChecked()) {
+                    Paper.book().write(Common.USER_KEY, email);
+                    Paper.book().write(Common.PWD_KEY, pass);
+                }
+                mAuthencation.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
@@ -109,23 +115,17 @@ public class LoginActivity extends AppCompatActivity {
                             mData.addValueEventListener(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                                    waiting.dismiss();
+                                    dlgWaiting.dismiss();
                                     User user = dataSnapshot.getValue(User.class);
-                                    Toast.makeText(LoginActivity.this, "Dang nhap thanh cong \n" + user.getName() , Toast.LENGTH_SHORT).show();
-
-//                                    if (user.getUserType().equals("admin")) {
+                                    if (user.getUserType().equals("admin")) {
 //                                        startActivity(new Intent(LoginActivity.this, AdminActivity.class));
-//                                    } else if (user.getUserType().equals("restaurent")) {
-//                                        startActivity(new Intent(LoginActivity.this, RestaurantActivity.class));
-//                                    } else if (user.getUserType().equals("customer")) {
-//
+                                    } else if (user.getUserType().equals("user")) {
 //                                        if (USER.isEmailVerified()) {
-//                                            startActivity(new Intent(LoginActivity.this, KhachHangActivity.class));
+                                            startActivity(new Intent(LoginActivity.this, CustomerActivity.class));
 //                                        } else {
 //                                            Toast.makeText(LoginActivity.this, "Vui lòng xác thực Email để đăng nhập", Toast.LENGTH_SHORT).show();
 //                                        }
-//
-//                                    }
+                                    }
 //                                    startActivity(new Intent(LoginActivity.this, AdminActivity.class));
                                 }
 
@@ -138,23 +138,24 @@ public class LoginActivity extends AppCompatActivity {
 
 
                             // ghi lai mk trong database neu quen mat kau sau khi lay lai
+                            if (!auto_login) {
+                                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("User").child(userID);
+                                ValueEventListener eventListener = new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        dataSnapshot.child("pass").getRef().setValue(pass);
+                                    }
 
-//                            DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
-//                            ValueEventListener eventListener = new ValueEventListener() {
-//                                @Override
-//                                public void onDataChange(DataSnapshot dataSnapshot) {
-//                                    dataSnapshot.child("pass").getRef().setValue(Pass);
-//                                }
-//
-//                                @Override
-//                                public void onCancelled(DatabaseError databaseError) {
-//
-//                                }
-//                            };
-//                            mDatabase.addListenerForSingleValueEvent(eventListener);
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                };
+                                mDatabase.addListenerForSingleValueEvent(eventListener);
+                            }
 
                         } else {
-//                            waiting.dismiss();
+                            dlgWaiting.dismiss();
                             Toast.makeText(LoginActivity.this, "Tài khoản hoặc mật khẩu không hợp lệ.", Toast.LENGTH_SHORT).show();
                         }
                     }
